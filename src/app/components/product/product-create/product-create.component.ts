d54e7c04f69d024cc4d9cde8bacbff4fbfe4e88f
Marcos Takao Barbosa <marcostkao@gmail.com>
2020-05-16 21:33:09 -0300

    Bindings

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
+  propLegal = 'qualquer'
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  fazerAlgo(): void {
+    console.log('Fazendo algo!');
+    
+  }
+
 
 }
