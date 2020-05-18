6fa0ce7cf1b82e966caff0dc0c52bad0676371a8
Marcos Takao Barbosa <marcostkao@gmail.com>
2020-05-18 07:29:27 -0300

    Método Criar Produto

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Router } from '@angular/router';
+import { Product } from '../product.module';
 
 @Component({
   selector: 'app-product-create',
@@ -8,6 +9,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
+
+  product: Product = {
+    name: '',
+    price: null
+  }
 
   constructor(private productService: ProductService,
     private router: Router) { }
@@ -17,7 +23,10 @@ export class ProductCreateComponent implements OnInit {
   }
 
   createProduct(): void {
-    this.productService.showMensage('Produto criado!')
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMensage('Produto criado!')
+      this.router.navigate(['/products'])
+    })
 
   }
 
