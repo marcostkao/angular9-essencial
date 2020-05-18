abf833e18a157f28a514bdcb31aaf0ef8a96bb32
Marcos Takao Barbosa <marcostkao@gmail.com>
2020-05-17 22:12:49 -0300

    Produto Service

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-create',
@@ -7,17 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  propLegal = 'qualquer'
-
-  constructor() { }
+  constructor(private productService: ProductService,
+    private router: Router) { }
 
   ngOnInit(): void {
+
   }
 
-  fazerAlgo(): void {
-    console.log('Fazendo algo!');
-    
+  createProduct(): void {
+    this.productService.showMensage('Produto criado!')
+
   }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+ 
 
 
 }
